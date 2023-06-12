f043fe7764221f94322a5ab4f77a5c8c745f3803
Lara Menck <lara.menckt@gmail.com>
2023-06-12 12:31:47 -0300

    course resouce findAll method

---
+++ src/main/java/com/project/crud/resources/CourseResource.java
@@ -0,0 +1,27 @@
+package com.project.crud.resources;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.crud.dtos.CourseDTO;
+import com.project.crud.services.CourseService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/courses")
+public class CourseResource {
+
+    private final CourseService service;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<CourseDTO>> findAll(){
+        return ResponseEntity.ok().body(service.findAll());
+    }
+    
+}
