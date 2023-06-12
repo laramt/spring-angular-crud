65c4b45db762c009729009e97e11b19df864ac22
Lara Menck <lara.menckt@gmail.com>
2023-06-12 15:32:40 -0300

    course data seeding

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO course_tb (name, description, professor, start_date, end_date) VALUES ('Calculus I', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', 'Sarah Doux', '2023-02-09', '2023-05-21');
+INSERT INTO course_tb (name, description, professor, start_date, end_date) VALUES ('Calculus II', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', 'Mandy Nouf', '2023-02-13', '2023-05-28');
