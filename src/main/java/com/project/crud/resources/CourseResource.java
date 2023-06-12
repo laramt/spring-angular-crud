7afc60adfa6a2fa591ce101005a33ac359b3faba
Lara Menck <lara.menckt@gmail.com>
2023-06-12 12:32:59 -0300

    course resource findById method

--- src/main/java/com/project/crud/resources/CourseResource.java
+++ src/main/java/com/project/crud/resources/CourseResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +23,11 @@ public class CourseResource {
     @GetMapping("/all")
     public ResponseEntity<List<CourseDTO>> findAll(){
         return ResponseEntity.ok().body(service.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CourseDTO> findById(@PathVariable Long id){
+        return ResponseEntity.ok().body(service.findById(id));
     }
     
 }
