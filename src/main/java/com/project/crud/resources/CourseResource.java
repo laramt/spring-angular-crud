b6043b19efe9ed44e74e4d208a467eb0105ef2a8
Lara Menck <lara.menckt@gmail.com>
2023-06-12 12:36:17 -0300

    course resource  insert method

--- src/main/java/com/project/crud/resources/CourseResource.java
+++ src/main/java/com/project/crud/resources/CourseResource.java
@@ -1,12 +1,16 @@
 package com.project.crud.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.project.crud.dtos.CourseDTO;
 import com.project.crud.services.CourseService;
@@ -19,6 +23,15 @@ import lombok.RequiredArgsConstructor;
 public class CourseResource {
 
     private final CourseService service;
+
+    @PostMapping("/new-course")
+    public ResponseEntity<CourseDTO> insert(@RequestBody CourseDTO dto){
+        dto = service.insert(dto);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
 
     @GetMapping("/all")
     public ResponseEntity<List<CourseDTO>> findAll(){
