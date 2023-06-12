b56fdabee34bcb49baa202da7fa5139baa365ae2
Lara Menck <lara.menckt@gmail.com>
2023-06-12 11:01:22 -0300

    model mapper dependency and course mapper

---
+++ src/main/java/com/project/crud/mappers/CourseMapper.java
@@ -0,0 +1,34 @@
+package com.project.crud.mappers;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import com.project.crud.dtos.CourseDTO;
+import com.project.crud.model.Course;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class CourseMapper {
+    
+    private final ModelMapper mapper;
+
+    public Course toCourse(CourseDTO dto){
+        return mapper.map(dto, Course.class);
+    }
+
+    public CourseDTO toCourseDTO(Course entity){
+        return mapper.map(entity, CourseDTO.class);
+    }
+
+    public List<CourseDTO> toCourseDTOList(List<Course> courses) {
+        return courses.stream()
+                .map(this::toCourseDTO)
+                .collect(Collectors.toList());
+    }
+
+}
