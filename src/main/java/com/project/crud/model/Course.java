850822eea8c75478f13912520e9e6272f277f925
Lara Menck <lara.menckt@gmail.com>
2023-06-12 15:33:03 -0300

    createdAt and updatedAt

--- src/main/java/com/project/crud/model/Course.java
+++ src/main/java/com/project/crud/model/Course.java
@@ -1,13 +1,17 @@
 package com.project.crud.model;
 
+import java.time.Instant;
 import java.time.LocalDate;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -31,4 +35,20 @@ public class Course {
 
     @JsonFormat(pattern = "dd-MM-yyyy")
     private LocalDate endDate;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant createdAt;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant updatedAt;
+
+    @PrePersist
+    public void prePersist(){
+        createdAt = Instant.now();
+    }
+
+    @PreUpdate
+    public void preUpdate(){
+        updatedAt = Instant.now();
+    }
 }
