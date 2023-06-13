f3b781f4b9dc05811c346427a2de1529d5511493
Lara Menck <lara.menckt@gmail.com>
2023-06-13 09:23:34 -0300

    ResourceNotFoundException

--- src/main/java/com/project/crud/exceptions/ResourceExceptionHandler.java
+++
@@ -1,9 +0,0 @@
-package com.project.crud.exceptions;
-
-import org.springframework.web.bind.annotation.ControllerAdvice;
-
-@ControllerAdvice
-public class ResourceExceptionHandler {
-    
-
-}
--- src/main/java/com/project/crud/exceptions/handler/StandardError.java
+++ src/main/java/com/project/crud/exceptions/handler/StandardError.java
@@ -1,4 +1,4 @@
-package com.project.crud.exceptions;
+package com.project.crud.exceptions.handler;
 
 import java.io.Serializable;
 import java.time.Instant;
