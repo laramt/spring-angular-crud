0a66527430000821cab63672034de25aca43fe9c
Lara Menck <lara.menckt@gmail.com>
2023-06-13 09:40:24 -0300

    InvalidInputException

--- src/main/java/com/project/crud/exceptions/handler/ResourceExceptionHandler.java
+++ src/main/java/com/project/crud/exceptions/handler/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.project.crud.exceptions.InvalidInputException;
 import com.project.crud.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -22,6 +23,16 @@ public class ResourceExceptionHandler {
         StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+    }
+
+    @ExceptionHandler(InvalidInputException.class)
+    public ResponseEntity<StandardError> invalidInput(InvalidInputException e, HttpServletRequest request){
+
+        String error = "Invalid input.";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
 
 
