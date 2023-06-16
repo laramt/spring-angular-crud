afc2773f81b3e5bcf7bbf30d1d3248bb33af8249
Lara Menck <lara.menckt@gmail.com>
2023-06-16 15:37:18 -0300

    Student model

---
+++ src/main/java/com/project/crud/model/Student.java
@@ -0,0 +1,35 @@
+package com.project.crud.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private LocalDate birthDate;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private String major;
+    
+}
