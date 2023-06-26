42e756ba3276eb9a08dfbbd9e97109d9d1cb85b0
laramenck <lara.menckt@gmail.com>
2023-06-26 16:06:03 -0300

    sudent service insert method tests

---
+++ backend/src/test/java/com/project/crud/services/serviceImpl/StudentServiceImplTest.java
@@ -0,0 +1,91 @@
+package com.project.crud.services.serviceImpl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.project.crud.builder.StudentBuilder;
+import com.project.crud.dtos.StudentDTO;
+import com.project.crud.exceptions.InvalidInputException;
+import com.project.crud.mappers.CourseMapper;
+import com.project.crud.mappers.StudentMapper;
+import com.project.crud.model.Student;
+import com.project.crud.repositories.StudentRepository;
+
+@SpringBootTest
+public class StudentServiceImplTest {
+
+    @Mock
+    private StudentRepository repository;
+
+    @Mock
+    private StudentMapper mapper;
+
+    @Mock
+    private CourseMapper courseMapper;
+
+    @InjectMocks
+    private StudentServiceImpl service;
+
+    @Test
+    public void insertStudentWIthValidRequestShouldReturnStudentDTO(){
+        // Arrange
+        Student student = StudentBuilder.createStudent();
+        StudentDTO dto = StudentBuilder.createStudentDTO();
+
+        when(mapper.toStudent(dto)).thenReturn(student);
+        when(repository.save(student)).thenReturn(student);
+        when(mapper.toStudentDTO(student)).thenReturn(dto);
+
+        // Act
+        StudentDTO result = service.insert(dto);
+
+        // Assert
+        assertNotNull(result);
+        assertEquals(dto.getId(), result.getId());
+        assertEquals(dto.getName(), result.getName());
+    }
+
+    @Test
+    public void insertCourseWithNullFildsShouldThrowInvalidInputException(){
+        // Arrange
+        Student student = StudentBuilder.createStudent();
+        StudentDTO dto = StudentBuilder.createStudentDTO();
+        student.setName(null);
+        student.setEmail(null);
+        student.setBirthDate(null);
+
+        when(mapper.toStudent(dto)).thenReturn(student);
+
+        // Assert
+        assertThrows(InvalidInputException.class, () -> service.insert(dto));
+        verify(mapper).toStudent(dto);
+
+    }
+
+    //@Test
+    public void insertCourseWithEmptyFildsShouldThrowInvalidInputException(){
+        // Arrange
+        Student student = StudentBuilder.createStudent();
+        StudentDTO dto = StudentBuilder.createStudentDTO();
+        student.setName("");
+        student.setEmail("");
+
+        when(mapper.toStudent(dto)).thenReturn(student);
+
+        // Assert
+        assertThrows(InvalidInputException.class, () -> service.insert(dto));
+        verify(mapper).toStudent(dto);
+
+    }
+
+
+
+}
